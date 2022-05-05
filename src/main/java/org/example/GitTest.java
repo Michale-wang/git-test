6991140439c2470f2c79f714e776d9357ddf0ec2
michale <2237982184@qq.com>
2022-05-06 03:59:43 +0800

    first commit

---
+++ src/main/java/org/example/GitTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Author Michale-
+ * @Date 2022/5/6 3:54
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
