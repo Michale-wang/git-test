962a29ef5dbd08838c2de199f87af7c940e6dc05
michale <2237982184@qq.com>
2022-05-06 04:22:01 +0800

    hot-fix change2

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test2");
-        System.out.println("hot-fix test");
+        System.out.println("hot-fix1 test");
     }
 }
