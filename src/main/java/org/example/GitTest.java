4fda761f0c3363a8b23c1d5634d5057315a0f301
michale <2237982184@qq.com>
2022-05-06 04:21:15 +0800

    master change2

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test2");
-        System.out.println("master test");
+        System.out.println("master1 test");
     }
 }
