ed78ff00d52ba6db8f70dace2938174679170da8
michale <2237982184@qq.com>
2022-05-06 04:01:50 +0800

    second commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -8,5 +8,6 @@ package org.example;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test2");
     }
 }
